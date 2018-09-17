c877a083141d318fd273acf0aa06af6340ee77a8
mishal089 <36955559+mishal089@users.noreply.github.com>
2018-09-17 15:06:30 +0300

    adding new pressure destrcutive artisanal fishing practices. added to pressure matrix and updated rank for other indicators for FIS goal. updated script to produce correct fields and file name.

--- prep/pressures/Destructive artisanal fishing practices/script_to_weight_illegal_gears.r
+++ prep/pressures/Destructive artisanal fishing practices/script_to_weight_illegal_gears.r
@@ -1,6 +1,9 @@
 library(plyr)
+library(here)
 
-illegal_gear<-read.csv(file = "prep/pressures/Destructive artisanal fishing practices/illegal gears count and score per county per year.csv",header = T,stringsAsFactors = F)
+setwd(here::here('prep//pressures/Destructive artisanal fishing practices/'))
+
+illegal_gear<-read.csv(file = "illegal gears count and score per county per year.csv",header = T,stringsAsFactors = F)
 
 unique(illegal_gear$Illegal.Gears)
 
@@ -21,8 +24,19 @@ illegal_summ<-ddply(illegal_gear,c('County',"Year"),summarise,
                     total_weighted_illegal=sum(weight_no))
 
 #new eighted proportion of illegal gears
-illegal_summ$weighted_prop<-round(illegal_summ$total_weighted_illegal/illegal_summ$total_weighted_gears,3)
+illegal_summ$pressure_score<-round(illegal_summ$total_weighted_illegal/illegal_summ$total_weighted_gears,3)
 
-final_results<-illegal_summ[,c(1,2,5)]
+illegal_summ$rgn_id<-NA
+illegal_summ$rgn_id[which(illegal_summ$County=='Mombasa')]<-1
+illegal_summ$rgn_id[which(illegal_summ$County=='Kwale')]<-2
+illegal_summ$rgn_id[which(illegal_summ$County=='Kilifi')]<-3
+illegal_summ$rgn_id[which(illegal_summ$County=='Tana River')]<-4
+illegal_summ$rgn_id[which(illegal_summ$County=='Lamu')]<-5
 
-write.csv(final_results,'proportion_of_illegal_gears_per_county.csv',row.names = F)
+
+final_results<-illegal_summ[,c(6,2,5)]
+
+final_results<-final_results[order(final_results$rgn_id),]
+
+
+write.csv(final_results,'fp_dest_art_ken2018.csv',row.names = F)
