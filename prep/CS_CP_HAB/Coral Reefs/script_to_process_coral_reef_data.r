a4052618b3804cdd1f9a3401973668110ce52720
mishal089 <36955559+mishal089@users.noreply.github.com>
2018-09-18 20:17:42 +0300

    updated the code to calculate trend and standardise values between -1 and 1.

--- prep/CS_CP_HAB/Coral Reefs/script_to_process_coral_reef_data.r
+++ prep/CS_CP_HAB/Coral Reefs/script_to_process_coral_reef_data.r
@@ -244,19 +244,25 @@ county_ave<-ddply(station_sum,c("Sector","Year","benthic_code"),summarise,
                   sd=sd(mean_cover),
                   n_sites=length(unique(Station)))
 
+#remove erronous time points Mombasa 2015, Kwale and Lamu 2016 and Kilifi 2014
+county_ave<-county_ave[-c(which(county_ave$Sector=='Kilifi'& county_ave$Year==2014),
+                                        which(county_ave$Sector=='Lamu'& county_ave$Year==2016),
+                                        which(county_ave$Sector=='Kwale'& county_ave$Year==2016),
+                                        which(county_ave$Sector=='Mombasa'& county_ave$Year==2015)),]
+
 #calculate trend - 5 most recent years for each county (contains some gaps in years)
-county_trend<-ddply(county_ave,c("Sector","benthic_code"),summarise,
-                    recent_cover=round(tail(ave_cover,n=6),2),
-                    sd=round(tail(sd,n=6),2),
-                    years=tail(Year,n=6),
-                    n=tail(n_sites,n=6))
+# county_trend<-ddply(county_ave,c("Sector","benthic_code"),summarise,
+#                     recent_cover=round(tail(ave_cover,n=6),2),
+#                     sd=round(tail(sd,n=6),2),
+#                     years=tail(Year,n=6),
+#                     n=tail(n_sites,n=6))
 
 write.csv(county_trend,"coral_cover_trend_6years_per_county.csv",row.names = F)
 
 library(dplyr)
 #calculate trends
 ## minimum year here for illustration; it is based on data available
-Year_min = 2011
+Year_min = 2006
 
 #
 r.trend <- county_ave %>%
@@ -270,7 +276,72 @@ r.trend <- county_ave %>%
 
 r.trend2 <- r.trend %>%
   group_by(Sector) %>%
-  do(mdl = lm(ave_cover ~ Year, data=.)) %>%
-  summarize( region_id = Sector,
-             trend = coef(mdl)['Year']*5) %>%
-  ungroup()
+  do(mdl = lm(ave_cover ~ Year, data=.),
+     adjust_trend = .$ave_cover[.$Year == min(.$Year)]) %>%
+  # summarize( region_id = Sector,
+  #            trend = coef(mdl)['Year']*5) %>%
+  # ungroup()%>%
+#script from OHI core
+
+dplyr::summarize(Sector, score = ifelse(coef(mdl)['Year']==0, 0, coef(mdl)['Year']/adjust_trend * 5))%>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(score = ifelse(score>1, 1, score)) %>%
+  dplyr::mutate(score = ifelse(score<(-1), (-1), score)) %>%
+  dplyr::mutate(score = round(score, 4)) %>%
+  dplyr::mutate(dimension = "trend") %>%
+  dplyr::select(Sector, score, dimension)
+
+r.trend2$rgn_id<-NA
+r.trend2$rgn_id[which(r.trend2$Sector=='Mombasa')]<-1
+r.trend2$rgn_id[which(r.trend2$Sector=='Kwale')]<-2
+r.trend2$rgn_id[which(r.trend2$Sector=='Kilifi')]<-3
+r.trend2$rgn_id[which(r.trend2$Sector=='Lamu')]<-5
+
+
+write.csv(trend2,"hab_coral_trend_ken2018.csv",row.names = F)
+
+# status_data<-county_ave
+#
+# status_data$rgn_id<-NA
+# status_data$rgn_id[which(status_data$Sector=='Mombasa')]<-1
+# status_data$rgn_id[which(status_data$Sector=='Kwale')]<-2
+# status_data$rgn_id[which(status_data$Sector=='Kilifi')]<-3
+# status_data$rgn_id[which(status_data$Sector=='Lamu')]<-5
+#
+# colnames(status_data)[2]<-'year'
+# colnames(status_data)[4]<-'status'
+#
+# CalculateTrend <- function(status_data, trend_years=trend_years){
+#
+#   if(sum(grepl("rgn_id", names(status_data))>0)){
+#     names(status_data)[which(names(status_data)=="rgn_id")] <- "region_id"
+#   }
+#
+#   if(sum(grepl("scenario_year", names(status_data)) > 0)) {
+#     names(status_data)[which(names(status_data) == "scenario_year")] <- "year"
+#   }
+#
+#   status_data <- status_data %>%
+#     dplyr::select(region_id, year, status) %>%
+#     dplyr::filter(year %in% trend_years) %>%
+#     unique()
+#
+#   adj_trend_year <- min(trend_years)
+#
+#   r.trend = status_data %>%
+#     dplyr::group_by(region_id) %>%
+#     dplyr::do(mdl = lm(status ~ year, data=.),
+#               adjust_trend = .$status[.$year == adj_trend_year]) %>%
+#     dplyr::summarize(region_id, score = ifelse(coef(mdl)['year']==0, 0, coef(mdl)['year']/adjust_trend * 5)) %>%
+#     dplyr::ungroup() %>%
+#     dplyr::mutate(score = ifelse(score>1, 1, score)) %>%
+#     dplyr::mutate(score = ifelse(score<(-1), (-1), score)) %>%
+#     dplyr::mutate(score = round(score, 4)) %>%
+#     dplyr::mutate(dimension = "trend") %>%
+#     dplyr::select(region_id, score, dimension)
+#
+#   return(r.trend)
+# }
+#
+# CalculateTrend(status_data)
+#
